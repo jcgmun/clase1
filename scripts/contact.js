15ad272d840d1bf9c368fe7755083157edc79667
jcgmun <jcgmunguia@gmail.com>
2025-11-12 15:08:31 -0500

    wip: empezamos validaciones nombre

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,8 +1,22 @@
 function init() {
   const contactForm = document.getElementById('contact-form');
+  const fields = {
+    name: {
+      element: document.getElementById('contact-form__name'),
+      validations: {
+        valueMissing: 'Please enter your name',
+      },
+    },
+    email: document.getElementById('contact-form__email'),
+    subject: document.getElementById('contact-form__subject'),
+    message: document.getElementById('contact-form__message'),
+  };
   contactForm.addEventListener('submit', (submitEvent) => {
     submitEvent.preventDefault();
     console.log(submitEvent);
+    console.log(fields);
+    const nameValidity = fields.name.element.validity;
+    console.log(nameValidity);
   });
 }
 init();
