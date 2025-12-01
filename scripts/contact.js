754a6ea4e8b00e16b650c7b014eef72d8e537745
jcgmun <jcgmunguia@gmail.com>
2025-11-30 20:44:58 -0500

    validity de todos los campos formulario

--- scripts/contact.js
+++ scripts/contact.js
@@ -7,16 +7,38 @@ function init() {
         valueMissing: 'Please enter your name',
       },
     },
-    email: document.getElementById('contact-form__email'),
-    subject: document.getElementById('contact-form__subject'),
-    message: document.getElementById('contact-form__message'),
+    email: {
+      element: document.getElementById('contact-form__email'),
+      validations: {
+        valueMissing: 'Please enter your email address',
+        typeMismatch: 'Please enter a valid email address',
+      },
+    },
+    subject: {
+      element: document.getElementById('contact-form__subject'),
+      validations: {
+        valueMissing: 'Please fill out the subject field',
+      },
+    },
+    message: {
+      element: document.getElementById('contact-form__message'),
+      validations: {
+        valueMissing: 'Please enter your message',
+      },
+    },
   };
   contactForm.addEventListener('submit', (submitEvent) => {
     submitEvent.preventDefault();
     console.log(submitEvent);
     console.log(fields);
     const nameValidity = fields.name.element.validity;
+    const emailValidity = fields.email.element.validity;
+    const subjectValidity = fields.subject.element.validity;
+    const messageValidity = fields.message.element.validity;
     console.log(nameValidity);
+    console.log(emailValidity);
+    console.log(subjectValidity);
+    console.log(messageValidity);
   });
 }
 init();
